f7fc7b8f367d8851b8d5a5ec7ae264f584741399
Tresor Tshimanga Kazadi <tshims79@gmal.com>
2022-05-19 14:19:53 +0200

    Pagination
    - I added the page link

--- ThedoorCode/Controllers/ProductController.cs
+++ ThedoorCode/Controllers/ProductController.cs
@@ -10,10 +10,17 @@ namespace ThedoorCode.Controllers
     public class ProductController : Controller
     {
         private IStoreRepository repository;
+
+        public int PageSize = 4;
         public ProductController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage = 1)
+                => View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
     }
 }
+
